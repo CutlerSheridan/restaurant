81e7c99cf689baea348c5b07c1be2ebfca13b836
Cutler Sheridan <cutler.sheridan@gmail.com>
2022-02-02 17:07:47 -0800

    0.1.0 Index; create all HTML with Javascript

--- README.md
+++ README.md
@@ -3,16 +3,20 @@
 Some joke about scum and villainy
 
 #### TO-DO NEXT
-
+- import createHomePage
 #### TO-DO LATER
 ##### Features
 ##### Behavior
 ##### Style
 - index; iron out responsiveness and breakpoints for tiles
+- index; iron out text box sizing for tiles
 ##### Other
 ##### Maybe
 
 #### DONE
+- *0.1.0*
+- index; create all HTML with javascript
+- expand flavor text
 - *0.0.2*
 - index; build small tiles with javascript
 - index; fiddle with tile styling
--- src/index.js
+++ src/index.js
@@ -1,11 +1,29 @@
-const createHomePage = (() => {
+const createHome = (() => {
+    const splashTile = document.createElement("div");
+    splashTile.id = "splash-tile";
+    const splashRestaurantName = document.createElement("h1");
+    splashRestaurantName.classList.add("splash-text");
+    splashRestaurantName.id = "restaurant-name";
+    splashRestaurantName.textContent = "Mos Eisley Cantina";
+    const splashRestaurantSlogan = document.createElement("h2");
+    splashRestaurantSlogan.classList.add("splash-text");
+    splashRestaurantSlogan.id = "restaurant-slogan";
+    splashRestaurantSlogan.textContent = "Live music! Cheap drinks!";
+    splashTile.append(splashRestaurantName, splashRestaurantSlogan);
+
+    const photoCaption = document.createElement("span");
+    photoCaption.classList.add("photo-caption");
+    photoCaption.textContent = "Figrin D'an and the Modal Nodes";
+
+    const gridContainer = document.createElement("section");
+    gridContainer.classList.add("grid-container");
     const smallTileContent = [
         "url(\"/src/index-images/drinks.jpg\")",
-        "cheap drinks lorum ipsum",
-        "live music lorum ipsum",
+        "Looking to get drunk cheap?  You won't find cheaper, stronger drinks anywhere on Tatooine!",
+        "Toast your success or drown your sorrows while listening to the hottest bands in town!",
         'url("/src/index-images/maz-band.jpg")',
         'url("/src/index-images/patron.jpeg")',
-        "all welcome lorum ipsum"
+        "We don't care what you are — Mind your own business and you're welcome anytime \n (No droids)"
     ];
     let smallTileTracker = 4;
     const fragment = document.createDocumentFragment();
@@ -26,6 +44,46 @@ const createHomePage = (() => {
         }
         fragment.append(newTile);
     }
-    const tilesGrid = document.querySelector(".grid-container");
-    tilesGrid.childNodes[0].before(fragment);
+    const testimonials = document.createElement("div");
+    testimonials.classList.add("big-tile");
+    testimonials.id = "testimonials";
+    const testimonialsArray = [
+        "I love it so much, my favorite bar",
+        "I do all my business dealings here",
+        "Can't be beat if you're looking for a good time",
+        "Not for the faint-hearted"
+    ];
+    for (let i = 0; i < 4; i++) {
+        const testimony = document.createElement("div");
+        testimony.classList.add("testimony");
+        testimony.textContent = testimonialsArray[i];
+        testimonials.append(testimony);
+    }
+    gridContainer.append(fragment, testimonials);
+
+    const homeFragment = document.createDocumentFragment();
+    homeFragment.append(splashTile, photoCaption, gridContainer);
+    return {homeFragment};
+})();
+
+const createBase = (() => {
+    const contentBase = document.querySelector("#content");
+
+    const header = document.createElement("header");
+    const tabHome = document.createElement("div");
+    tabHome.classList.add("active-tab");
+    tabHome.id = "tab-home";
+    tabHome.textContent = "About";
+    const tabMenu = document.createElement("div");
+    tabMenu.id = "tab-menu";
+    tabMenu.textContent = "Menu";
+    const tabContact = document.createElement("div");
+    tabContact.id = "tab-contact";
+    tabContact.textContent = "Contact";
+    const tabs = [tabHome, tabMenu, tabContact];
+    tabs.forEach(tab => {
+        tab.classList.add("tab");
+        header.append(tab);
+    });
+    contentBase.append(header, createHome.homeFragment);
 })();
