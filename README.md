c9e6c6b41f609dcb2ec365af445dea9570ffaef5
Cutler Sheridan <cutler.sheridan@gmail.com>
2022-02-03 12:55:42 -0800

    0.1.1 Implement JS importing

--- README.md
+++ README.md
@@ -3,8 +3,11 @@
 Some joke about scum and villainy
 
 #### TO-DO NEXT
-- import createHomePage
+- process images with webpack into bundle
+- make tabs clickable
+- add logic to clear content from #content
 #### TO-DO LATER
+- separate css into separate files that get packaged into one?
 ##### Features
 ##### Behavior
 ##### Style
@@ -14,6 +17,10 @@ Some joke about scum and villainy
 ##### Maybe
 
 #### DONE
+- *0.1.1*
+- revert index.html to read from packaged main.js
+- import createHomePage
+- add auto-repackaging
 - *0.1.0*
 - index; create all HTML with javascript
 - expand flavor text
---
+++ src/home-content.js
@@ -0,0 +1,69 @@
+const createHome = (() => {
+    const splashTile = document.createElement("div");
+    splashTile.id = "splash-tile";
+    const splashRestaurantName = document.createElement("h1");
+    splashRestaurantName.classList.add("splash-text");
+    splashRestaurantName.id = "restaurant-name";
+    splashRestaurantName.textContent = "Mos Eisley Cantina";
+    const splashRestaurantSlogan = document.createElement("h2");
+    splashRestaurantSlogan.classList.add("splash-text");
+    splashRestaurantSlogan.id = "restaurant-slogan";
+    splashRestaurantSlogan.textContent = "Live music! Cheap drinks!";
+    splashTile.append(splashRestaurantName, splashRestaurantSlogan);
+
+    const photoCaption = document.createElement("span");
+    photoCaption.classList.add("photo-caption");
+    photoCaption.textContent = "Figrin D'an and the Modal Nodes";
+
+    const gridContainer = document.createElement("section");
+    gridContainer.classList.add("grid-container");
+    const smallTileContent = [
+        "url(\"/src/index-images/drinks.jpg\")",
+        "Looking to get drunk cheap?  You won't find cheaper, stronger drinks anywhere on Tatooine!",
+        "Toast your success or drown your sorrows while listening to the hottest bands in town!",
+        'url("/src/index-images/maz-band.jpg")',
+        'url("/src/index-images/patron.jpeg")',
+        "We don't care what you are — Mind your own business and you're welcome anytime \n (No droids)"
+    ];
+    let smallTileTracker = 4;
+    const fragment = document.createDocumentFragment();
+    for (let i = 0; i < smallTileContent.length; i++) {
+        const newTile = document.createElement("div");
+        newTile.classList.add("small-tile");
+        if (smallTileTracker > 2) {
+            newTile.classList.add("img-tile");
+            newTile.style.backgroundImage = smallTileContent[i];
+        } else {
+            newTile.classList.add("desc-tile");
+            newTile.textContent = smallTileContent[i];
+        }
+        if (smallTileTracker === 4) {
+            smallTileTracker = 1;
+        } else {
+            smallTileTracker++;
+        }
+        fragment.append(newTile);
+    }
+    const testimonials = document.createElement("div");
+    testimonials.classList.add("big-tile");
+    testimonials.id = "testimonials";
+    const testimonialsArray = [
+        "I love it so much, my favorite bar",
+        "I do all my business dealings here",
+        "Can't be beat if you're looking for a good time",
+        "Not for the faint-hearted"
+    ];
+    for (let i = 0; i < 4; i++) {
+        const testimony = document.createElement("div");
+        testimony.classList.add("testimony");
+        testimony.textContent = testimonialsArray[i];
+        testimonials.append(testimony);
+    }
+    gridContainer.append(fragment, testimonials);
+
+    const homeFragment = document.createDocumentFragment();
+    homeFragment.append(splashTile, photoCaption, gridContainer);
+    return {homeFragment};
+})();
+
+export default createHome;
